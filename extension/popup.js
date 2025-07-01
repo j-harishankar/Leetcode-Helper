cba33c7b458c320b310b3c33e81ad2151e241229
jydv402 <jydv402@gmail.com>
2025-07-02 01:14:25 +0530

    Added loading animation

--- extension/popup.js
+++ extension/popup.js
@@ -1,37 +1,49 @@
 console.log("📦 popup.js loaded");
 
 document.getElementById("getHelp").addEventListener("click", async () => {
-  chrome.tabs.query({ active: true, currentWindow: true }, async (tabs) => {
-    const tab = tabs[0];
-    const url = tab.url;
-    const title = tab.title;
+  const output = document.getElementById("output");
+  const loader = document.getElementById("loader");
 
-    console.log("LeetCode URL:", url);
-    console.log("Problem Title:", title);
+  //show loader
+  loader.style.display = "block";
+  output.innerHTML = "";
 
-    const response = await fetch("http://127.0.0.1:8000/api/solve/", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json"
-      },
-      body: JSON.stringify({ title, url })
-    });
+  try {
+    chrome.tabs.query({ active: true, currentWindow: true }, async (tabs) => {
+      const tab = tabs[0];
+      const url = tab.url;
+      const title = tab.title;
 
-    const data = await response.json();
-    console.log("✅ Response from Django:", data);
+      console.log("LeetCode URL:", url);
+      console.log("Problem Title:", title);
 
-    const output = document.getElementById("output");
-    output.innerHTML = "";
-
-    if (data.steps && Array.isArray(data.steps)) {
-      data.steps.forEach((step) => {
-        const div = document.createElement("div");
-        div.innerHTML = marked.parse(step); // Using Marked package
-        output.appendChild(div);
+      const response = await fetch("http://127.0.0.1:8000/api/solve/", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify({ title, url })
       });
-    } else {
-      output.innerHTML = "<p>No steps found.</p>";
-    }
 
-  });
+      const data = await response.json();
+      console.log("✅ Response from Django:", data);
+
+      // hide loader
+      loader.style.display = "none";
+
+      if (data.steps && Array.isArray(data.steps)) {
+        data.steps.forEach((step) => {
+          const div = document.createElement("div");
+          div.innerHTML = marked.parse(step); // Render markdown
+          output.appendChild(div);
+        });
+      } else {
+        output.innerHTML = "<p>No steps found.</p>";
+      }
+    });
+  } catch (err) {
+    loader.style.display = "none";
+    output.innerHTML = "<p style='color: red;'>Error fetching data. Try again?</p>";
+    console.error("❌ Error:", err);
+  }
 });
